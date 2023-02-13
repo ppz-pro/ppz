916d2af596b43721f2f69e33276ef78234f92127
gy <372301467@qq.com>
2023-02-13 19:46:49 +0800

    feat: 前端基本布局

---
+++ frontend/src/entry/app.jsx
@@ -0,0 +1,15 @@
+export default
+function App() {
+  return <div className = 'app'>
+    <Left />
+    <Pages />
+  </div>
+}
+
+function Left() {
+  return <div>left</div>
+}
+
+function Pages() {
+  return <div>pages</div>
+}
